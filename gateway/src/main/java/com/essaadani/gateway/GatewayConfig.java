6a2a46dd1a8ed789219d8682ef32c3fbea4fc0bf
Mohamed Es-saadani <essaadani80@gmail.com>
2021-11-01 22:35:19 +0100

    commit

--- gateway/src/main/java/com/essaadani/gateway/GatewayConfig.java
+++ gateway/src/main/java/com/essaadani/gateway/GatewayConfig.java
@@ -4,7 +4,9 @@ import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class GatewayConfig {
     @Bean
     DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(
