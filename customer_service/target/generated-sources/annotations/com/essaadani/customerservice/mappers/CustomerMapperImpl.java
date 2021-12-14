986cf979fff343f760b8333c2d4201d74267ec9d
Mohamed Es-saadani <essaadani80@gmail.com>
2021-12-14 21:49:53 +0100

    'commit

--- customer_service/target/generated-sources/annotations/com/essaadani/customerservice/mappers/CustomerMapperImpl.java
+++ customer_service/target/generated-sources/annotations/com/essaadani/customerservice/mappers/CustomerMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-11-20T12:31:32+0100",
+    date = "2021-12-14T21:45:32+0100",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.11 (Oracle Corporation)"
 )
 @Component
